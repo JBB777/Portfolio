190169eca010c17c34ce95a4b013e32d3846f02f
JBB <jbaptistebesancon@gmail.com>
2025-03-03 13:06:41 +0100

    Ajout bouton vers les repo git depuis la section projets

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Gallery.scss';
 import projects from '../../datas/projects.json';
 import Card from '../../components/Card/Card';
@@ -21,6 +22,14 @@ function Gallery() {
             />
           );
         })}
+        <div className="gallery__gitLink">
+          <Link
+            to={'https://github.com/JBB777?tab=repositories'}
+            target="_blank"
+          >
+            + sur Git.
+          </Link>
+        </div>
       </div>
     </section>
   );
